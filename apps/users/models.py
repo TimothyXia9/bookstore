a4dc01ae1158242be5834b890becaa992de4ea91
TimothyXia9 <77732791+TimothyXia9@users.noreply.github.com>
2023-07-09 16:55:29 +0800

    fixed setting recipient

--- apps/users/models.py
+++ apps/users/models.py
@@ -54,6 +54,9 @@ class Recipient(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def delete_recipient(self):
+        self.delete()
 
 
 class Order(models.Model):
